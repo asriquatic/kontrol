1f7ae56656263711289827c085be0b4888534d72
asriquatic <asriquaticsales@gmail.com>
2025-10-16 07:55:10 +0700

    postcss.config.js

--- postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-// postcss.config.js atau postcss.config.cjs
-// Pastikan ini menggunakan sintaks ES Module atau CommonJS yang benar
-module.exports = {
-  plugins: {
-    // PostCSS modern mencari paket secara otomatis berdasarkan nama key
-    'tailwindcss': {},
-    'autoprefixer': {},
-  }
-};
---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {}, // Sintaks objek sederhana
+    autoprefixer: {},
+  }
+};

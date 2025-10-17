4314cfe0a81bb1ebf3daabeebf86b4d92f4db0e4
asriquatic <asriquaticsales@gmail.com>
2025-10-17 10:39:23 +0700

    Fix: Finalisasi konfigurasi build dan rilis aplikasi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
-// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-export default {
-  // PENTING: Tentukan file mana yang harus dipindai Tailwind
+module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // Memindai semua file React Anda
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
